375d3ddaf57fde0666cdbcc5e879a73aafb986c6
Eduardo Jaime <eduardojaime86@gmail.com>
2026-07-07 20:29:58 -0400

    l09 showing products filtered by category

--- TechJockeys/Controllers/StoreController.cs
+++ TechJockeys/Controllers/StoreController.cs
@@ -24,19 +24,35 @@ namespace TechJockeys.Controllers
             return View(categories);
         }
 
+        // This method returns a page with the products belonging to that category
         public IActionResult ByCategory(int id)
         {
+
             // error handle if id missing => redirect to Store index so user can choose a category
-            if (id == 0 || id > 15)
+            if (id <= 0)
             {
                 return RedirectToAction("Index");
             }
 
+            // Retrieve list of products from the DB
+            var products = _context.Product                         // FROM PRODUCT p
+                                    .Where(p => p.CategoryId == id) // WHERE p.CategoryId = @id 
+                                    .OrderBy(p => p.Name)           // ORDER BY p.Name
+                                    .ToList();                      // SELECT *
+
+            // Retrieve the category name to show on the page in the title
+            var category = _context.Category.Find(id);
+            // redirect to index if category is null
+            if (category == null) {
+                return RedirectToAction("index");
+            }
+
             // use id param to find category
             // use ViewData dictionary to show selected category name in heading
-            ViewData["Category"] = "Category " + id.ToString();
+            // since category is nullable, question mark '?' will make this value empty on runtime if null
+            ViewData["Category"] = category?.Name;
       
-            return View();
+            return View(products);
         }
     }
 }
